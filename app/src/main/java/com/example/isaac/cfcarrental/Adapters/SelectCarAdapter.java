bc10b1c1eccc91b3c10e04ba6f768940738f1451
synarc1 <isaacsilas05@gmail.com>
2018-11-06 17:02:51 +1100

    Recycler View set up with dummy values

--- app/src/main/java/com/example/isaac/cfcarrental/Adapters/SelectCarAdapter.java
+++ app/src/main/java/com/example/isaac/cfcarrental/Adapters/SelectCarAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.isaac.cfcarrental.R;
 
@@ -32,16 +33,35 @@ public class SelectCarAdapter extends RecyclerView.Adapter<SelectCarAdapter.Sele
     @Override
     public void onBindViewHolder(@NonNull SelectCarViewHolder selectCarViewHolder, int i) {
 
+        selectCarViewHolder.fuelType.setText(mList.get(i).getFuelType().toString());
+        selectCarViewHolder.carSeat.setText(mList.get(i).getCarSeat().toString());
+        selectCarViewHolder.model.setText(mList.get(i).getModel().toString());
+        selectCarViewHolder.manufacture.setText(mList.get(i).getManufacture().toString());
+        selectCarViewHolder.plateNumber.setText(mList.get(i).getPlateNumber().toString());
+        selectCarViewHolder.price.setText(mList.get(i).getPrice().toString());
+        selectCarViewHolder.milage.setText(mList.get(i).getMilage().toString());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mList.size();
     }
 
     public class SelectCarViewHolder extends RecyclerView.ViewHolder{
+
+        TextView plateNumber, price, milage, manufacture, model, carSeat, fuelType;
+
         public SelectCarViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            plateNumber = itemView.findViewById(R.id.plateNumber);
+            price = itemView.findViewById(R.id.price);
+            milage = itemView.findViewById(R.id.mileage);
+            manufacture = itemView.findViewById(R.id.manufacturer);
+            model = itemView.findViewById(R.id.model);
+            carSeat = itemView.findViewById(R.id.carSeat);
+            fuelType = itemView.findViewById(R.id.fuelType);
         }
     }
 }
